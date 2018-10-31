be277000f2deefee63535bd10be4cf553ddaf080
Nian <2038770992@qq.com>
2018-10-31 22:26:26 +0800

    重新架构

--- Online/urls.py
+++ Online/urls.py
@@ -1,7 +1,7 @@
-"""Django URL Configuration
+"""Online URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,9 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-#网址入口，关联到对应的views.py中的一个函数（或者generic类），访问网址就对应一个函数。
-from Django.views import hello
-from django.conf.urls import url
-import sys
+from django.contrib import admin
+from django.urls import path
 
-print(sys.path)
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
--- Online/wsgi.py
+++ Online/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for Django project.
+WSGI config for Online project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Django.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Online.settings')
 
 application = get_wsgi_application()
